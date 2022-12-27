e1df5185a8b9596f8d22ce9397e07b8318689e45
Rilind UBT <rilindubt@Rilinds-MacBook-Pro.local>
2022-12-27 03:40:53 +0100

    fix: bring back logs

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,7 @@ const app: Application = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
-// app.use(morgan('dev', { stream }))
+app.use(morgan('dev', { stream }))
 
 // passport
 app.use(
