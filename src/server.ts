5f98e91251b6708c16d0d80418961fa7a9698f0f
Rilind UBT <rilindubt@Rilinds-MacBook-Pro.local>
2023-04-02 23:36:14 +0200

    fix: return 200 for preflights

--- src/server.ts
+++ src/server.ts
@@ -123,9 +123,7 @@ app.use(express.json())
 // }
 // app.options('*', cors(corsOptions))
 
-app.options('/*', (_, res) => {
-  res.sendStatus(200)
-})
+app.options('/*', (_, res) => res.sendStatus(200))
 
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', 'http://localhost:3000') // update to match the domain you will make the request from
